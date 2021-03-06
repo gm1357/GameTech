275af648d6071363e2f194c73dc162f5224a76f3
Gabriel Machado <g_machado1@hotmail.com>
2021-03-05 23:10:05 -0300

    Added tabs to game details screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -4,46 +4,112 @@ import 'package:gametech/models/game.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class DetailsScreen extends StatelessWidget {
+class DetailsScreen extends StatefulWidget {
   static const routeName = '/details';
+
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen>
+    with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(
+      length: 2,
+      vsync: this,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     final Game game = ModalRoute.of(context).settings.arguments;
+    final _tabs = [
+      GameDescription(game),
+      GameGallery(),
+    ];
+
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: 300,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text(
-                game.name,
-                overflow: TextOverflow.ellipsis,
-              ),
-              background: Hero(
-                tag: '${game.name}-cover',
-                child: Image.network(
-                  game.imageDetail,
-                  fit: BoxFit.cover,
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverOverlapAbsorber(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              sliver: SliverAppBar(
+                expandedHeight: 200,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Hero(
+                    tag: '${game.name}-cover',
+                    child: Image.network(
+                      game.imageDetail,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                bottom: TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.description)),
+                    Tab(icon: Icon(Icons.photo)),
+                  ],
+                  controller: controller,
                 ),
               ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              SizedBox(height: 10),
-              game.description != null
-                  ? Html(
-                      data: '${game.description}',
-                      onLinkTap: (String relativePath) async =>
-                          await _launchURL(
-                              'https://www.giantbomb.com$relativePath'))
-                  : Text('${game.deck}'),
-            ]),
-          ),
-        ],
+          ];
+        },
+        body: TabBarView(
+          controller: controller,
+          children: _tabs.map((Widget widget) {
+            return SafeArea(
+              top: false,
+              bottom: false,
+              child: Builder(
+                builder: (BuildContext context) {
+                  return CustomScrollView(
+                    key: PageStorageKey<String>(widget.key.toString()),
+                    slivers: <Widget>[
+                      SliverOverlapInjector(
+                        handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                            context),
+                      ),
+                      SliverPadding(
+                        padding: const EdgeInsets.all(8.0),
+                        sliver: SliverList(
+                          delegate: SliverChildListDelegate([widget]),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            );
+          }).toList(),
+        ),
       ),
+    );
+  }
+}
+
+class GameDescription extends StatelessWidget {
+  final Game game;
+
+  GameDescription(this.game);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        game.description != null
+            ? Html(
+                data: '${game.description}',
+                onLinkTap: (String relativePath) async =>
+                    await _launchURL('https://www.giantbomb.com$relativePath'))
+            : Text('${game.deck}'),
+      ],
     );
   }
 
@@ -53,5 +119,14 @@ class DetailsScreen extends StatelessWidget {
     } else {
       throw 'Could not launch $url';
     }
+  }
+}
+
+class GameGallery extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Text('teste')],
+    );
   }
 }
